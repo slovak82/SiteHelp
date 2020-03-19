08abcc2319a4fcb9058a27d2149f0e7822302061
Viacheslav <clavik15@gmail.com>
2020-03-19 18:30:04 +0200

    Fixed the bug with the menu
    The menu won't close automatically in mobile version

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    $(".header__link").click(function () {
+        $('.header__burger,.header__menu').removeClass("active");
+        $('body').removeClass('lock');
+    });
 });
 
 // $(".header_link").click(function () {
